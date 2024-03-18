4d6f61577d98a2c2c18188e54a89f059d3ba6477
Hitesh Behera <bhiteshme74@gmail.com>
2024-03-18 23:54:29 +0530

    import: necessary packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -7,3 +8,5 @@ const App = () => {
     </div>
   );
 }
+
+export default App;
