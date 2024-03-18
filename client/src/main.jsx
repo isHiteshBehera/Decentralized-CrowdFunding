affefcd5cb618497c3f7f43e8083bceaa8b453ef
Hitesh Behera <bhiteshme74@gmail.com>
2024-03-18 21:28:31 +0530

    create: Vite App

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import "./styles/globals.css";
 
-import { StateContextProvider } from './context';
-import App from './App';
-import './index.css';
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "ethereum";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
-    <Router>
-      <StateContextProvider>
-        <App />
-      </StateContextProvider>
-    </Router>
-  </ThirdwebProvider> 
-)
+  <React.StrictMode>
+    <ThirdwebProvider
+      clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
+      activeChain={activeChain}
+    >
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
