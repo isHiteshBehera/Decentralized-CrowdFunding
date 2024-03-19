d4e6be54e535ee80f014bd1e6e514c4d19c13da3
Hitesh Behera <bhiteshme74@gmail.com>
2024-03-19 08:16:19 +0530

    fix: tailwindcss

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily: {
+        epilogue: ['Epilogue', 'sans-serif'],
+      },
+      boxShadow: {
+        secondary: '10px 10px 20px rgba(2, 2, 2, 0.25)',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
