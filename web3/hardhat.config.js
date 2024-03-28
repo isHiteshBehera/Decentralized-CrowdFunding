ce376b09949ac5030d0a93daf365d05df22ed8f0
Hitesh Behera <bhiteshme74@gmail.com>
2024-03-28 11:13:44 +0530

    update: hardhat configuration

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -37,8 +37,8 @@ module.exports = {
     networks : {
       hardhat: {},
       sepoli: {
-        url: 'https://rpc.ankr.com/eth_sepolia',
-        accounts: ['0x${process.env.PRIVATE_KEY}']
+        url: "https://sepolia.rpc.thirdweb.com",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
       },
     },
     settings: {
